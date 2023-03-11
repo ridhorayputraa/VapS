19634771cd42a13b1d673cfcc2dbef9737efe3d8
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-11 13:34:20 +0700

    editing env from mailtrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,6 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,6 @@
 
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
