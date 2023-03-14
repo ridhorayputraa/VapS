383835e77fa6b79141d6074a6819c2a8a58570b0
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-14 15:43:51 +0700

    adding filtering by id limit name and types

--- app/Http/Controllers/API/VapeController.php
+++ app/Http/Controllers/API/VapeController.php
@@ -9,8 +9,17 @@ class VapeController extends Controller
 {
     //
     public function all(Request $request){
-          // 1 fungsi ini  akan menghandle semua request
+     // 1 fungsi ini  akan menghandle semua request
     // perlu menyiapkan beberapa opsi untuk filter
     // Berdasarkan ID, Berdasarlan Harga, Berdasarkan Tipe
+
+    $id = $request->input('id');
+    $limit = $request->input('limit',6);
+    $name = $request->input('name');
+    $types = $request->input('types');
+
+
+    // Berdasarkan harga dari yang terkecil -> terbesar
+    
     }
 }
