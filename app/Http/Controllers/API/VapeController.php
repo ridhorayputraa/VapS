bd739db67ce8e8a4dd5cde21a2b39095b9844382
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-14 15:40:12 +0700

    adding VapsController for product API

---
+++ app/Http/Controllers/API/VapeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VapeController extends Controller
+{
+    //
+    public function all(Request $request){
+          // 1 fungsi ini  akan menghandle semua request
+    // perlu menyiapkan beberapa opsi untuk filter
+    // Berdasarkan ID, Berdasarlan Harga, Berdasarkan Tipe
+    }
+}
