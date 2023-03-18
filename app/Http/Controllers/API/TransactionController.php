75aaf68566d84109920ce5bed7ec5c74d03530b6
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-18 19:54:04 +0700

    konfigurasi midtrans

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -108,6 +108,11 @@ class TransactionController extends Controller
             // akan di update setelah nembak midtras
         ]);
 
-      }
+        // Konfigurasi Midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+    }
 
 }
