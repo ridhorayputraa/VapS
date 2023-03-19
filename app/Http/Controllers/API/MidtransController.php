eeb016f3316adbfa11d831957d326d0571e00dd5
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-19 09:08:02 +0700

    create callback method and adding config for midtrans for and assign the notification than handle the notification parameter

--- app/Http/Controllers/API/MidtransController.php
+++ app/Http/Controllers/API/MidtransController.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use Midtrans\Config;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Transaction;
+use Midtrans\Notification;
 
 class MidtransController extends Controller
 {
     //
+    public function callback(Request $request)
+    {
+       // Set Konfigurasi Midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+       //  Buat Instance midtrans Notification
+       $notif = new Notification();
+
+       // Assign Ke variable untuk memudahkan coding
+        $status = $notif->transcation_status;
+        $type = $notif->payment_type;
+        $fraud = $notif->fraud_status;
+        $order_id = $notif->order_id;
+
+       // Cari transaksi berdasarkan ID
+       $transaction = Transaction::findOrFail($order_id);
+
+      //    handle Notifikasi status midtrans
+     
+     //   Simpan Transaksi
+    }
 }
