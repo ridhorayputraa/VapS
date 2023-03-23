1ea4a1f6df0d8437ddf19dcf3b3e9f1bf4e70a41
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-24 05:32:24 +0700

    edited index file for data vape

--- resources/views/vape/index.blade.php
+++ resources/views/vape/index.blade.php
@@ -1,16 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('User') }}
+            {{ __('Vape') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mb-10">
-                <a href="{{ route('users.create') }}"class="bg-green-500 hover:bg-green-700 text-white
+                <a href="{{ route('vape.create') }}"class="bg-green-500 hover:bg-green-700 text-white
                 font-bold py-2 px-4 rounded " >
-                 + Create User
+                 + Create Product
                  </a>
             </div>
             {{-- Untuk Table --}}
@@ -20,25 +20,28 @@
                         <tr>
                             <th class="border px-6 py-4">ID</th>
                             <th class="border px-6 py-4">Name</th>
-                            <th class="border px-6 py-4">Email</th>
-                            <th class="border px-6 py-4">Roles</th>
+                            <th class="border px-6 py-4">Price</th>
+                            <th class="border px-6 py-4">Rate</th>
+                            <th class="border px-6 py-4">Types</th>
                             <th class="border px-6 py-4">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse ($user as $item )
+                        @forelse ($vape as $item )
                         <tr>
                             <td class="border px-6 py-4">{{ $item->id }}</td>
                             <td class="border px-6 py-4">{{ $item->name }}</td>
-                            <td class="border px-6 py-4">{{ $item->email }}</td>
-                            <td class="border px-6 py-4">{{ $item->roles }}</td>
+                            {{-- Price nya di ubah jadi number format agar lebih rapih --}}
+                            <td class="border px-6 py-4">{{ number_format($item->price) }}</td>
+                            <td class="border px-6 py-4">{{ $item->rate }}</td>
+                            <td class="border px-6 py-4">{{ $item->types }}</td>
                             <td class="border px-6 py-4 text-center">
-                                <a href="{{ route('users.edit', $item->id) }}"
+                                <a href="{{ route('vape.edit', $item->id) }}"
                                      class="inline-block bg-blue-500 hover:bg-blue-700 text-white
                                      font-bold py-2 px-4 mx-2 rounded
                                      ">Edit</a>
                                      {{-- Form Hapus Untuk Delete--}}
-                                     <form action="{{ route('users.destroy', $item->id) }}"  method="POST" class="inline-block">
+                                     <form action="{{ route('vape.destroy', $item->id) }}"  method="POST" class="inline-block">
                                        {!! method_field('delete') . csrf_field() !!}
                                     <button type="submit" class="bg-red-500 hover:bg-red-700 text-white
                                     font-bold py-2 px-4 mx-2 rounded
@@ -50,7 +53,7 @@
                         </tr>
                         @empty
                             <tr>
-                                <td colspan="5" class="border text-center p-5">
+                                <td colspan="6" class="border text-center p-5">
                                     Data Tidak Ditemukan
                                 </td>
                             </tr>
@@ -60,7 +63,7 @@
             </div>
             {{-- Buat Link/Pagination --}}
             <div class="text-center mt-5">
-                {{ $user->links() }}
+                {{ $vape->links() }}
             </div>
         </div>
     </div>
